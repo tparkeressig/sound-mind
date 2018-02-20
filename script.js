1e1fc84c150deaddf6fdd65f490231cc916a4789
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-02-20 14:35:56 -0800

    Basic File Structure for HTML, CSS, and JS complete

---
+++ script.js
@@ -0,0 +1,35 @@
+// SoundMind project, begun Feb 2018
+
+console.log("Reading JS");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+            console.log("DOM fully loaded and parsed");
+
+            var canvas = document.getElementsByTagName('canvas');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        });
